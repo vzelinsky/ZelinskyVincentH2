2133e6477e796b5a3f0d473d74c1d42dda16f1a4
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-09-29 23:59:03 -0400

    Initial commit, creates Recursion.java which will serve as a testing class for recursive classes.

---
+++ Recursion.java
@@ -0,0 +1,10 @@
+/* Recursion.java
+ * Written by Vincent Zelinsky
+ * 
+ * A class to test recursive classes and principles
+ */
+public class Recursion {
+  public static void main (String[] args){
+    
+  }
+}
