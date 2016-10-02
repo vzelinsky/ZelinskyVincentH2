634fefb2816db124bd45251dd712dd490f42dcdd
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-01 22:44:39 -0400

    Created empty Babylonian.java

---
+++ Babylonian.java
@@ -0,0 +1,8 @@
+/* Babylonian.java
+ * Written by Vincent Zelinsky
+ * 
+ * A class to approximate a square root of a given number within a certain margin of error using the Babylonian method
+ */
+public class Babylonian {
+  
+}
