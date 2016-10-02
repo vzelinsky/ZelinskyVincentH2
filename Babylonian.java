f8d4721d3f509db9c743b9787a6f216f0953e723
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-01 23:50:51 -0400

    Added constructor for Babylonian which accepts a double

--- Babylonian.java
+++ Babylonian.java
@@ -4,8 +4,16 @@
  * A class to approximate a square root of a given number within a certain margin of error using the Babylonian method
  */
 public class Babylonian {
+  
+  //Private field number
   private double number;
   
+  //Creates a constructor that accepts a double value
+  public Babylonian(double input){
+    this.number = input;
+  }
+  
+  //Getter method for number
   public double getNumber(){
     return number;
   }
