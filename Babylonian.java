0276a2c71b78bd80ebe6bf68b2411e7053680aa3
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-02 00:16:02 -0400

    Added error field and another constructor to pass a value to it

--- Babylonian.java
+++ Babylonian.java
@@ -3,15 +3,23 @@
  * 
  * A class to approximate a square root of a given number within a certain margin of error using the Babylonian method
  */
+import java.util.Scanner;
+
 public class Babylonian {
   
   //Private field number
   private double number;
   private double guess;
+  private double error;
   
   //Creates a constructor that accepts a double value
   public Babylonian(double input){
     this.number = input;
+  }
+  
+  public Babylonian(double numberInput, double errorInput) {
+    this.number = numberInput;
+    this.error = errorInput*0.01;
   }
   
   //Getter method for number
@@ -21,7 +29,13 @@ public class Babylonian {
   
   //Creates a method that will calculate square root
   public double calcSquareRoot(){
-    guess = 0.5*number;
-    return guess;
+    if (number == 0) {
+      return 0;
+    }
+    else {
+      guess = number*0.5;
+      return guess;
+    }
+    
   }
 }
