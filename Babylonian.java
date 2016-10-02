6b0ab16c400be9d6409d127d66563d31dcf262dc
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-02 00:48:58 -0400

    Creates a convience method

--- Babylonian.java
+++ Babylonian.java
@@ -14,12 +14,15 @@ public class Babylonian {
   private double guess;
   private double error;
   
-  //Creates a constructor that accepts a double value
+  //Creates a no argument constructor that prompts the user for field values
   public Babylonian(){
     System.out.println("Please enter the number you would like to find the square root of: ");
     this.number = scanner.nextDouble(); 
+    System.out.println("Please enter acceptable accuracy(0.1,0.01,etc): ");
+    this.error = scanner.nextDouble();
   }
   
+  //Constructor that accepts number and error
   public Babylonian(double numberInput, double errorInput) {
     this.number = numberInput;
     this.error = errorInput;
@@ -30,14 +33,22 @@ public class Babylonian {
     return number;
   }
   
-  //Creates a method that will calculate square root
+  //Convience method
   public double calcSquareRoot(){
+    return calcSquareRoot(number,error);
+  }
+  
+  //Creates a method that will calculate square root
+  public double calcSquareRoot(double numberInput, double errorInput){
+    this.number = numberInput;
+    this.error = errorInput;
+    
     if (Math.abs((number - (guess*guess))) < error ) {
       return guess;
     }
     else {
       guess = number*0.5;
-      return guess;
+      return calcSquareRoot(guess,error);
     }
     
   }
