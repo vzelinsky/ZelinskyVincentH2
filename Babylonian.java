281ab7ff2725e55f956bcd98bd5eff11276306c5
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-01 23:40:25 -0400

    Added the field number and a getter for the double value

--- Babylonian.java
+++ Babylonian.java
@@ -4,5 +4,9 @@
  * A class to approximate a square root of a given number within a certain margin of error using the Babylonian method
  */
 public class Babylonian {
+  private double number;
   
+  public double getNumber(){
+    return number;
+  }
 }
