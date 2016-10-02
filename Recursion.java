3fec5f6a73245ede75e7a45aeffc3f46c749e6c4
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-01 23:51:23 -0400

    imports Scanner util and creates a Scanner and Babylonian object

--- Recursion.java
+++ Recursion.java
@@ -3,8 +3,16 @@
  * 
  * A class to test recursive classes and principles
  */
+import java.util.Scanner;
+
 public class Recursion {
+  
   public static void main (String[] args){
+    
+    //Creates a Scanner object called scanner
+    Scanner scanner = new Scanner(System.in);
+    //Creates a Babylonian object called squareRoot
+    Babylonian squareRoot = new Babylonian(9);
     
   }
 }
