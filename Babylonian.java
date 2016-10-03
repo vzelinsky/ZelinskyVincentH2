eea181802418ffd0211df53c5fb32c2d63879a93
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-02 21:41:36 -0400

    Fixes comments

--- Babylonian.java
+++ Babylonian.java
@@ -9,21 +9,30 @@ public class Babylonian {
   //Creates scanner object
   Scanner scanner = new Scanner(System.in);
   
-  //Private field number
+  //Private fields
+  //number to find the square root of
   private double number;
+  //initial guess
   private double iguess;
+  //new guess (the recursive guess)
   private double nguess;
+  //error
   private double error;
   
-  //Creates a no argument constructor that prompts the user for field values
+  //Creates a no argument constructor that when called prompts the user for field values
+  /* If I were to redo this program I would instead make setters for the fields number and error.
+   * I would put the scanner calls in the convenience method calcSquareRoot instead and set the values before proceeding.
+   */ 
   public Babylonian(){
+    //Prompts for number field
     System.out.println("Please enter the number you would like to find the square root of: ");
-    this.number = scanner.nextDouble(); 
-    System.out.println("Please enter acceptable accuracy(within 10,1,0.1,0.01,etc): ");
+    this.number = scanner.nextDouble();
+    //Prompts for error field
+    System.out.println("Please enter acceptable accuracy(within 10,1,0.1,0.01,etc of square): ");
     this.error = scanner.nextDouble();
   }
   
-  //Constructor that accepts number and error
+  //Constructor that accepts number and error as arguments
   public Babylonian(double numberInput, double errorInput) {
     this.number = numberInput;
     this.error = errorInput;
@@ -31,18 +40,21 @@ public class Babylonian {
   
   //Convenience method
   public double calcSquareRoot(){
+    //Initial guess is half of the number
     iguess = number*0.5;
+    //Calls itself and overloades by passing the inital guess as an argument
     return calcSquareRoot(iguess);
   }
   
-  //Overloades the calcSquareRoot method to calculate square root
+  //Overloaded calcSquareRoot method that will recursively calculate square root
   public double calcSquareRoot(double nguessInput){
+    //Sets nguess as the value passed to this method
     this.nguess = nguessInput;
-    //If the difference between number and nguess^2 is less than error then print nguess
+    //Base case: If the difference between number and nguess^2 is less than error then return nguess
     if (Math.abs((number - (nguess*nguess))) < error ) {
       return nguess;
     }
-    //Then recursive call
+    //Recursive call: Calculates and sets new guess using the old guess and passes the value to itself(the method)
     else {
       nguess = (nguess+(number/nguess))*0.5;
       return calcSquareRoot(nguess);
