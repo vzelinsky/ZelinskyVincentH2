9f1eccfe5a22cef3615ed19afc7e7beb4a6e9706
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-10-02 21:40:55 -0400

    Didn't need scanner in main

--- Recursion.java
+++ Recursion.java
@@ -3,14 +3,9 @@
  * 
  * A class to test recursive classes and principles
  */
-import java.util.Scanner;
-
 public class Recursion {
   
   public static void main (String[] args){
-    
-    //Creates a Scanner object called scanner
-    Scanner scanner = new Scanner(System.in);
     //Creates a Babylonian object called squareRoot and passes 9 to it
     Babylonian squareRoot = new Babylonian();
     
